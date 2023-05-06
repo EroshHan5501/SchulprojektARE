70c5faea4206785801bb3274978973d74db38e62
Eren UEzer <eren.uezer@theobald-software.com>
2023-05-06 17:02:28 +0200

    Use CLI Parameters

--- Pokedex/ApiCaller/Program.cs
+++ Pokedex/ApiCaller/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = String.Empty;
+            string apiString = "https://pokeapi.co/api/v2/";
+            if (args.Length > 0)
+            {
+                input = args[0];
+            }
+
+            HttpClient client = new HttpClient();
+            using HttpResponseMessage response = client.GetAsync($"{apiString}{input}").Result;
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseBody);
         }
     }
 }
