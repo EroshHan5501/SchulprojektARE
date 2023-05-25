7fdbee335e2d88270de7424d51d1ec1d5ef792ea
Eren UEzer <eren.uezer@theobald-software.com>
2023-05-25 22:40:14 +0200

    Deserialize Pokemon Names, URL and Moves to Pokemon Class;

--- Pokedex/ApiCaller/Program.cs
+++ Pokedex/ApiCaller/Program.cs
@@ -20,43 +20,71 @@ namespace ApiCaller
             string responseBody = response.Content.ReadAsStringAsync().Result;
             Console.WriteLine(responseBody);
 
+
             //Deserialize json-string
             //Testing:
             Pages seite = JsonSerializer.Deserialize<Pages>(responseBody);
             Console.WriteLine(seite.version);
 
             //Actual:
-            var bulba= JsonSerializer.Deserialize<Results>(responseBody);
+            Results pokemonList = JsonSerializer.Deserialize<Results>(responseBody);
+
             Console.WriteLine("***** ***** *****");
-            foreach(Pokemon pokemon in bulba.results) 
+            if (pokemonList!= null && pokemonList.results!= null)
             {
-                Console.WriteLine(pokemon.name);
-                Console.WriteLine(pokemon.url);
+                foreach(Pokemon pokemon in pokemonList.results) 
+                {
+                    Console.WriteLine(pokemon.name);
+                    Console.WriteLine(pokemon.url);
+
+                    HttpClient clientMoves = new HttpClient();
+                    using HttpResponseMessage responseMoves = client.GetAsync($"{pokemon.url}").Result;
+                    string responseBodyMoves = responseMoves.Content.ReadAsStringAsync().Result;
+                    //Console.WriteLine(responseBodyMoves);
+                    //Console.WriteLine(pokemon.url);
+                    MoveList moveList = JsonSerializer.Deserialize<MoveList>(responseBodyMoves);
+                    pokemon.moves = moveList.moves;
+                    foreach (MoveStats mS in pokemon.moves)
+                    {
+                        Console.Write($"__{mS.move.name}");
+                    }
+                    Console.WriteLine("__");
+                    //Results pokemonList = JsonSerializer.Deserialize<Results>(responseBody);
+                }
             }
-            Console.WriteLine(bulba.results);
             Console.WriteLine("***** ***** *****");
         }
 
         //Testing:
         public class Pages
         {
-            public string version { get; set; }
+            public string? version { get; set; }
         }
 
         //Actual:
         public class Pokemon
         {
-            public string name { get; set; }
-            public string url { get; set; }
-            public List<Moves> moves { get; set; } 
+            public string? name { get; set; }
+            public string? url { get; set; }
+            public List<MoveStats>? moves { get; set; } 
         }
         public class Results
         {
-            public List<Pokemon> results { get; set; }
+            public List<Pokemon>? results { get; set; }
         }
-        public class Moves
+        public class Move
         {
+            public string? name { get; set; }
+            public string? url { get; set; }
+        }
+        public class MoveStats
+        {
+            public Move? move { get; set; }
+        }
 
+        public class MoveList
+        {
+            public List<MoveStats>? moves { get; set; }
         }
     }
 }
