5d65b802c70382e23cfef224089ba731ccb084aa
Eren UEzer <eren.uezer@theobald-software.com>
2023-06-30 10:08:33 +0200

    Add using to Class

--- Pokedex/Common/Entities/Pokemon.cs
+++ Pokedex/Common/Entities/Pokemon.cs
@@ -1,6 +1,7 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using MySqlConnector;
 using Pokedx.Common;
 
