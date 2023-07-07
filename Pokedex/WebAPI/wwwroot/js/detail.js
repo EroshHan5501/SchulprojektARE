86e14002cccd8539deb3b4f8ee430fd577386836
AnnikaGommel <annikagommel2207@gmail.com>
2023-07-07 19:41:13 +0200

    detail seite angepasst

--- Pokedex/WebAPI/wwwroot/js/detail.js
+++ Pokedex/WebAPI/wwwroot/js/detail.js
@@ -8,19 +8,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     const id = url.searchParams.get('pokemonId');
     const response = await fetch(`https://localhost:7212/api/Pokemon/detail/${id}`);
     const json = await response.json();
-    document.getElementById("kp").innerText(json.kp);
-    document.getElementById("number").innerText(json.number);
-    document.getElementById("type").innerText(json.type);
-    document.getElementById("height").innerText(json.height);
-    document.getElementById("pokemonImage").innerText(json.pokemonImage);
-    document.getElementById("basicstad").innerText(json.basicstad);
-    document.getElementById("specialstad").innerText(json.specialstad);
-    document.getElementById("extrastad").innerText(json.extrastad);
+    document.getElementById("number").innerText("Nummer: "+json.id);
+    document.getElementById("type").innerText("Typ: " + json.type);
+    document.getElementById("height").innerText("Größe: " + json.height);
+    document.getElementById("pokemonImage").setAttribute("src", json.pokemonImage);
+
+    document.getElementById("kp").innerText(json.hp);
+    document.getElementById("attackstat").innerText(json.hp);
+    document.getElementById("defstat").innerText(json.hp);
+    document.getElementById("spattackstat").innerText(json.hp);
+    document.getElementById("spdefstat").innerText(json.hp);
+    document.getElementById("speedstat").innerText(json.hp);
+
+    let abilities = json.abilis;
+    let abilityList = document.getElementById("abilities");
+    for (let ability in abilities) {
+        let ab = document.createElement("li");
+        ab.innerText = ability.name + " | " + ability.entry;
+        abilityList.appendChild(ab);
+    }
 
     let attacks = document.getElementById("attacks")
-    for(let attack in json.attacks) {
+    for(let attack in json.movement) {
         let newAttack = document.createElement('li');
-        newAttack.innerText = attack;
+        newAttack.innerText = attack.name;
         attacks.appendChild(newAttack)
     }
 
